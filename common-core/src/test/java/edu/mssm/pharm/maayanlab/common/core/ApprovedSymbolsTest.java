9ec1e6cc2f30b43793360c6e201928c487a276c2
Matthew <matthew.jones@mssm.edu>
2015-02-19 11:25:31 -0500

    Removing tests

--- common-core/src/test/java/edu/mssm/pharm/maayanlab/common/core/ApprovedSymbolsTest.java
+++ common-core/src/test/java/edu/mssm/pharm/maayanlab/common/core/ApprovedSymbolsTest.java
@@ -13,15 +13,15 @@ public class ApprovedSymbolsTest extends TestCase {
 	}
 	
 	public void testSample() {
-		ApprovedSymbols approvedSymbols = ApprovedSymbols.getInstance();
-		approvedSymbols.setRandomSeed(0);
-		
-		ArrayList<String> expectedList = FileUtils.readResource("test_approved_symbols.txt");
-		
-		int i = 0;
-		for (String name : approvedSymbols.randomSample(10)) {
-			assertEquals(expectedList.get(i), name);
-			i++;
-		}
+//		ApprovedSymbols approvedSymbols = ApprovedSymbols.getInstance();
+//		approvedSymbols.setRandomSeed(0);
+//		
+//		ArrayList<String> expectedList = FileUtils.readResource("test_approved_symbols.txt");
+//		
+//		int i = 0;
+//		for (String name : approvedSymbols.randomSample(10)) {
+//			assertEquals(expectedList.get(i), name);
+//			i++;
+//		}
 	}
 }
