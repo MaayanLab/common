bf0552977d257d7244d4ff76df7aa4f424bfc76d
Daniel J. B. Clarke <u8sand@gmail.com>
2018-06-22 16:23:00 -0400

    Added readTokens to PartReader

--- common-web/src/main/java/edu/mssm/pharm/maayanlab/common/web/PartReader.java
+++ common-web/src/main/java/edu/mssm/pharm/maayanlab/common/web/PartReader.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 import javax.servlet.http.Part;
 
@@ -16,6 +17,25 @@ public class PartReader {
 		String line = null;
 		while ((line = fileReader.readLine()) != null)
 			inputList.add(line.trim());
+		fileReader.close();
+		
+		return inputList;
+	}
+
+	public static ArrayList<String> readTokens(Part chunk) throws IOException {
+		ArrayList<String> inputList = new ArrayList<String>();
+		
+		BufferedReader fileReader = new BufferedReader(new InputStreamReader(chunk.getInputStream()));
+		String line = fileReader.readLine();
+		while (line != null) {
+			StringTokenizer tk = new StringTokenizer(line);
+			String token = tk.nextToken();
+			while (token != null) {
+				inputList.add(token.trim());
+				token = tk.nextToken();
+			}
+			line = fileReader.readLine();
+		}
 		fileReader.close();
 		
 		return inputList;
